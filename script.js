4683af3b92a684355b7344003b65e77c05488011
David Conde <dctoo42me@gmail.com>
2022-10-06 21:03:36 -0500

    remove prompt and add ui to game

--- script.js
+++ script.js
@@ -19,12 +19,49 @@ let playerScore = 0;
 let computerScore = 0;
 let ties = 0;
 
+let results = document.querySelector('.results');
+
+let playersScore = document.createElement('p');
+let playerDiv = document.querySelector('#player-score');
+playerDiv.appendChild(playersScore);
+
+let scoreBoard = document.querySelector('#score-board');
+
+let compScore = document.createElement('p');
+let compDiv = document.querySelector('#comp-score');
+compDiv.appendChild(compScore);
+
+let gameTies = document.createElement('p');
+let tiesDiv = document.querySelector('#ties');
+tiesDiv.appendChild(gameTies);
+
+let buttons = document.querySelector('.buttons');
+let btn = document.querySelectorAll('.btn');
+let playAgain = document.querySelector('#play-again');
+
+let start = document.querySelector('div.start button');
+// start.style.marginTop = '20px';
+let startDiv = document.querySelector('.start-game');
+
 //game fuctions
 function getComputerChoice(array) {
     let randomNum = array[Math.floor(Math.random() * array.length)];
     return randomNum;
 }
-
+(function initialState() {
+    buttons.classList.add('hidden');
+})();
+function startGame() {
+    btn.disabled = false;
+    scoreBoard.classList.remove('hidden');
+    results.classList.remove('hidden');
+    startDiv.classList.add('hidden');
+    buttons.classList.remove('hidden');
+    console.log('its working');
+}
+function gameOver() {
+    buttons.classList.add('hidden');
+}
 function playRound(playerSelection, computerSelection) {
     if(
     playerSelection === r && computerSelection === r ||
@@ -32,65 +69,72 @@ function playRound(playerSelection, computerSelection) {
     playerSelection === pp && computerSelection === pp 
     ) {
         ties++;
-        return "It's a tie!";
+        results.textContent =  "It's a tie!";
     } else if(
         playerSelection === r && computerSelection === pp
     )   {
         computerScore++;
-        return `You Lose this round, ${paperOverRock}!`;
+        results.textContent =  `You lose round, ${paperOverRock}!`;
     } else if(
         playerSelection === r && computerSelection === s
     )   {
         playerScore++;
-        return `You Win this round, ${rockOverScissors}!`;
+        results.textContent =  `You win round, ${rockOverScissors}!`;
     } else if(
         playerSelection === pp && computerSelection === r
     )   {
         playerScore++;
-        return `You Win this round, ${paperOverRock}`;
+        results.textContent =  `You win round, ${paperOverRock}`;
     } else if(
         playerSelection === pp && computerSelection === s 
     )   {
         computerScore++;
-        return `You Lose this round, ${scissorsOverPaper}!`;
+        results.textContent =  `You lose round, ${scissorsOverPaper}!`;
     } else if(
         playerSelection === s && computerSelection === pp 
     )   {
         playerScore++;
-        return `You Win this round, ${scissorsOverPaper}!`;
+        results.textContent =  `You win round, ${scissorsOverPaper}!`;
     } else if(
         playerSelection === s && computerSelection === r 
     )   {
         computerScore++;
-        return `You Lose this round, ${rockOverScissors}!`;
+        results.textContent =  `You lose round, ${rockOverScissors}!`;
     } 
     else {
         console.log("error, something is wrong");
-        return 'Something is wrong with your code';
+        results.textContent =  'Something is wrong with your code';
     }
+
+    playersScore.textContent = `${playerScore}`;
+    compScore.textContent = `${computerScore}`;
+    gameTies.textContent = `${ties}`;
+
+    if( playerScore === 5) {
+        // buttons.style.display = 'none';
+        results.textContent = 'YOU WIN! GAME OVER!';
+        playAgain.classList.remove('hidden');
+        playAgain.classList.add('unhide');
+        gameOver();        
+    } else if ( computerScore === 5) {
+        // buttons.style.display = 'none';
+        results.textContent = 'YOU LOST! GAME OVER!!';
+        playAgain.classList.remove('hidden');
+        playAgain.classList.add('unhide');
+        gameOver();
+    }   
 }
 
-function game() {
-    for(let i =0; i < 5; i++) {
-        playerChoice = prompt('rock, paper, or scissors? ');
-        playerSelection = playerChoice.toLowerCase();
-        computerSelection = getComputerChoice(choicesArray);
-        console.log(playRound(playerSelection,computerSelection));
-        console.log('i: ', i);
-        console.log('player:', playerSelection, 'computer:', computerSelection);
-        console.log('p-score:', playerScore, 'c-score:', computerScore);
-        console.log('*****************************');
-    }
-    if( playerScore > computerScore) {
-        console.log('YOU WIN! GAME OVER!');
-    } else if ( computerScore > playerScore) {
-        console.log('YOU LOST! GAME OVER!!')
-    }   else {
-        console.log("IT'S A TIE!, GAME OVER!!!")
-    }
-        console.log('ties: ', ties);
-        console.log('comp: ', computerScore);
-        console.log('player: ', playerScore);
-}
 //call game function
-game();
+let rockBtn = document.querySelector('#rock');
+rockBtn.addEventListener('click', () => playRound('rock', getComputerChoice(choicesArray)));
+
+let paperBtn = document.querySelector('#paper');
+paperBtn.addEventListener('click', () => playRound('paper', getComputerChoice(choicesArray)));
+
+let scissorsBtn = document.querySelector('#scissors');
+scissorsBtn.addEventListener('click', () => playRound('scissors', getComputerChoice(choicesArray)));
+
+start.addEventListener('click', startGame);
+
+playAgain.addEventListener('click', () => window.location.reload());
